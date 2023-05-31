949c70bb800092223cca270fabdb80b482c7bc63
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2023-05-31 15:29:30 +0800

    Remove repo token
    Signed-off-by: Ivan Vanderbyl <ivanvanderbyl@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# actions
-Ensignia GitHub Actions
+# upload-provenance-action
+
+Ensignia Provenance Action
+
+Usage:
+
+```yaml
+steps:
+  build:
+    permissions:
+      id-token: write # To sign the provenance.
+      contents: write # To upload assets to release.
+      actions: read # To read the workflow path.
+      checks: read
+    needs: args
+    uses: slsa-framework/slsa-github-generator/.github/workflows/builder_go_slsa3.yml@v1.6.0
+    with:
+      go-version: 1.20.3
+
+  publish:
+    needs: build
+    runs-on: ubuntu-latest
+    steps:
+      - name: Download artifact
+        uses: actions/download-artifact@v3
+        with:
+          name: ${{ needs.build.outputs.go-provenance-name }}
+
+      - uses: ensigniasec/upload-provenance-action@main
+        with:
+          api-key: "${{ secrets.ENSIGNIA_API_KEY }}"
+          repo-token: ${{ secrets.GITHUB_TOKEN }}
+          provenance-name: "${{ needs.build.outputs.go-provenance-name }}"
+```
--- main.go
+++ main.go
@@ -28,11 +28,6 @@ func realMain(ctx context.Context) error {
 		gha.Fatalf("api-key input param is required")
 	}
 
-	// ghToken := gha.GetInput("repo-token")
-	// if apiKey == "" {
-	// 	gha.Fatalf("repo-token input param is required")
-	// }
-
 	provenanceName := gha.GetInput("provenance-name")
 	if provenanceName == "" {
 		gha.Fatalf("provenance-name input param is required (e.g. 'needs.build.outputs.provenance-name')")
@@ -45,52 +40,6 @@ func realMain(ctx context.Context) error {
 
 	gha.Infof("Provenance: %s", string(prov)[0:100])
 
-	// ghContext, err := gha.Context()
-	// if err != nil {
-	// 	return err
-	// }
-
-	// owner, repo := ghContext.Repo()
-	// runID := ghContext.RunID
-
-	// client := gh.NewTokenClient(ctx, ghToken)
-	// var list *gh.ArtifactList
-
-	// fn := func(ctx context.Context) error {
-	// 	results, _, err := client.Actions.ListWorkflowRunArtifacts(ctx, owner, repo, runID, &gh.ListOptions{})
-	// 	if err != nil {
-	// 		return retry.RetryableError(err)
-	// 	}
-
-	// 	if results.GetTotalCount() == 0 {
-	// 		gha.Warningf("Nothing found, retrying...")
-
-	// 		return retry.RetryableError(fmt.Errorf("no artifacts found"))
-	// 	}
-
-	// 	gha.Infof("Found %d artifacts", results.GetTotalCount())
-
-	// 	list = results
-	// 	return nil
-	// }
-
-	// bo := retry.WithMaxRetries(5, retry.NewExponential(time.Second))
-	// err = retry.Do(ctx, bo, fn)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// gha.Infof("Found %d artifacts", list.GetTotalCount())
-
-	// for _, artifact := range list.Artifacts {
-	// 	if artifact.GetName() == provenanceName {
-	// 		gha.Infof("Found Artifact: Attestation %s", artifact.GetName())
-	// 	}
-
-	// 	gha.Infof("Artifact: %s", artifact.GetName())
-	// }
-
-	// setOutput("url", "https://console.ensignia.dev/")
 	return nil
 }
 
